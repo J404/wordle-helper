0746b827ee90d432c1f4196cf96eda3f0ae03eb7
J404 <jimmystry@gmail.com>
2022-02-04 00:15:26 -0500

    Add files via upload

---
+++ index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { solveWordle } from './src/index';
+
+    export let currGuess: string;
+    export let orangeLetters: string;
+    export let notLetters: string;
+
+    function runSolver() {
+        solveWordle(currGuess, orangeLetters, notLetters);
+    }
+</script>
+
+
+<h1>wordlde thing</h1>
+
+<input bind:value={currGuess} placeholder="green letters">
+<input bind:value={orangeLetters} placeholder="orange letters">
+<input bind:value={notLetters} placeholder="not letters">
+
+<button on:click={runSolver}>Solve</button>
